41bca70e7ff2c95e9e5b131a2f7138fa765ec7fe
César Ivan Delgado Silva <ceides9497@gmail.com>
2017-04-15 01:47:42 -0400

    Completa V.1

--- procesar.php
+++ procesar.php
@@ -1,11 +1,13 @@
 <?php
+  $error = null;
+  //error_reporting(0);
   copy($_FILES['archivo']['tmp_name'],$_FILES['archivo']['name']);
-  echo "Archivo guardado exitosamente";
+  //echo "Archivo guardado exitosamente";
   $nombre =  $_FILES['archivo']['name'];
-  echo $nombre."<br />" ;
-  echo $_FILES['archivo']['type']."<br />";
+  //echo $nombre."<br />" ;
+  //echo $_FILES['archivo']['type']."<br />";
   if ($_FILES['archivo']['type']=="text/plain") {
-    echo "yeah";
+    //echo "yeah";
     rename($nombre, "mi_fichero.txt");
   }
 
@@ -20,11 +22,13 @@
           while (($line = fgets($handle)) !== false) {
               $reg = explode("|", $line);
               //genera codigo de control
+              $fecha = str_replace('/', '-',$reg[2]);
+              $fecha = date("Y-m-d",strtotime($fecha));
               $code = $controlCode->generate($_POST['authorizationNumber'],//Numero de autorizacion
-                                             $reg[1],//Numero de factura
-                                             $reg[2],//Número de Identificación Tributaria o Carnet de Identidad
-                                             str_replace('/','',$reg[3]),//fecha de transaccion de la forma AAAAMMDD
-                                             $reg[4],//Monto de la transacción
+                                              $reg[3],//Numero de factura
+                                              $reg[6],//Número de Identificación Tributaria o Carnet de Identidad
+                                              str_replace('-','',$fecha),//fecha de transaccion de la forma AAAAMMDD
+                                              $reg[14],//Monto de la transacción
                                              $_POST['dosageKey']//Llave de dosificación
                       );
               fwrite($fi,$line."=>".$code.PHP_EOL);
@@ -34,8 +38,11 @@
            throw new Exception("<b>Could not open the file!</b>");
       }
   }catch ( Exception $e ){
-       echo "Error (File: ".$e->getFile().", line ".
-            $e->getLine()."): ".$e->getMessage();
+    $error = true;
+    echo "<center>
+    <h1>Error!!!</h1>
+    <b>Asegurese de haber ingresado los datos correctos</b><br /><br />
+    </center>";
   }
   fclose($fi);
 ?>
@@ -46,8 +53,20 @@
     <title>Resultado</title>
   </head>
   <body>
-    <form action="descargar.php" method="post" enctype="multipart/form-data">
-      <input type="submit" name="submit" value="Descargar">
-    </form>
+    <?php if (!$error): ?>
+      <center>
+        <h1>Exito!!!</h1>
+        <b>Ahora puede descargar el archivo.</b><br><br>
+        <form action="descargar.php" method="post" enctype="multipart/form-data">
+          <input type="submit" name="submit" value="Descargar">
+        </form>
+        <br>
+        <button type="button" name="button"><a href="main.php" style="color:black;  text-decoration:none;">Volver</a></button>
+      </center>
+    <?php else: ?>
+      <center>
+        <button type="button" name="button"><a href="main.php" style="color:black;  text-decoration:none;">Volver</a></button>
+      </center>
+    <?php endif; ?>
   </body>
 </html>
