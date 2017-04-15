c6c60f319f0f435ab2d5d5be9954cbcc1bc439f7
César Ivan Delgado Silva <ceides9497@gmail.com>
2017-04-15 00:26:59 -0400

    casi

--- main.php
+++ main.php
@@ -6,15 +6,23 @@
 </head>
 <body>
 <h1>Calcular</h1>
-<form action="procesar.php" method="post" enctype="multipart/form-data">
+<form action="procesar.php" method="post" enctype="multipart/form-data" id="form">
     <label for="">Número de autorización:</label>
     <input type="text" name="authorizationNumber" value="" required=""><br><br>
     <label for="">Llave de dosificación:</label>
     <input type="text" name="dosageKey" value="" size="65" required=""><br><br>
     <label for="archivo">Seleccionar archivo a subir:</label>
-    <input type="file" name="archivo" id="archivo" required=""><br><br>
-    <input type="submit" value="Upload" name="submit">
+    <input type="file" name="archivo" id="archivo" accept="text/plain"><br><br>
+    <input type="submit" value="Upload" name="submit" required="" onclick="return comprueba_extension(this.form, this.form.archivo.value)">
 </form>
-
+<script type="text/javascript">
+    function comprueba_extension(formulario, archivo){
+        extension = (archivo.substring(archivo.lastIndexOf("."))).toLowerCase();
+        if(extension == ".txt")
+            return true;
+        alert("Debe inserta un archivo .txt obligatoriamente, Gracias.");
+        return false;
+    }
+</script>
 </body>
 </html>
--- procesar.php
+++ procesar.php
@@ -6,9 +6,48 @@
   echo $_FILES['archivo']['type']."<br />";
   if ($_FILES['archivo']['type']=="text/plain") {
     echo "yeah";
-  }else {
-    echo "nope";
-    echo "<script language="javascript">window.location='main.php';</script>";
+    rename($nombre, "mi_fichero.txt");
   }
-  rename($nombre, "mi_fichero.txt");
+
+  $fi = fopen("resultado.txt","w+") or die ("Problemas al recuperar el archivo");
+  include 'codigocontrol.php';
+  try{
+      $filename="mi_fichero.txt";
+      $handle = fopen($filename, "r");
+      if ($handle) {
+          $controlCode = new ControlCode();
+          $count=0;
+          while (($line = fgets($handle)) !== false) {
+              $reg = explode("|", $line);
+              //genera codigo de control
+              $code = $controlCode->generate($_POST['authorizationNumber'],//Numero de autorizacion
+                                             $reg[1],//Numero de factura
+                                             $reg[2],//Número de Identificación Tributaria o Carnet de Identidad
+                                             str_replace('/','',$reg[3]),//fecha de transaccion de la forma AAAAMMDD
+                                             $reg[4],//Monto de la transacción
+                                             $_POST['dosageKey']//Llave de dosificación
+                      );
+              fwrite($fi,$line."=>".$code.PHP_EOL);
+          }
+      fclose($handle);
+      }else{
+           throw new Exception("<b>Could not open the file!</b>");
+      }
+  }catch ( Exception $e ){
+       echo "Error (File: ".$e->getFile().", line ".
+            $e->getLine()."): ".$e->getMessage();
+  }
+  fclose($fi);
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Resultado</title>
+  </head>
+  <body>
+    <form action="descargar.php" method="post" enctype="multipart/form-data">
+      <input type="submit" name="submit" value="Descargar">
+    </form>
+  </body>
+</html>
